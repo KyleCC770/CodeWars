ad5425c330004e88fc61441150e36cdfb120a75f
Kyle Collins <kyle.collins7737@gmail.com>
2022-06-24 22:05:22 -0400

    added count the divisors of a number codewars challenge

---
+++ 7kyu/count-the-divisors-of-a-number.js
@@ -0,0 +1,13 @@
+/*
+Given an Number (n) count the number of divisors 
+*/
+
+function getDivisorsCnt(n) {
+  let output = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      output += 1;
+    }
+  }
+  return output;
+}
